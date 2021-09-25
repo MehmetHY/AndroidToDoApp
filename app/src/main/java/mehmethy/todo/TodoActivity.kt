43f66d8e3323569092e13111969dffd3598afa24
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-09-25 11:47:51 +0300

    created widget class, add icons

--- app/src/main/java/mehmethy/todo/TodoActivity.kt
+++ app/src/main/java/mehmethy/todo/TodoActivity.kt
@@ -1,37 +1,25 @@
 package mehmethy.todo
 
 import android.os.Bundle
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
-import android.widget.ScrollView
 import androidx.appcompat.app.AppCompatActivity
+import mehmethy.todo.widget.TodoWidget
+import mehmethy.todo.widget.activeTodoWidget
 
 class TodoActivity : AppCompatActivity() {
     private val todoContainer: LinearLayout by lazy(LazyThreadSafetyMode.NONE) { findViewById(R.id.todoContainer) }
+    private val todoList: MutableList<TodoWidget> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_todo)
 
         val addButton = findViewById<Button>(R.id.addButton)
-        addButton.setOnClickListener { createTodo() }
-    }
-
-    private fun createTodo() {
-        val layout = LinearLayout(this)
-        layout.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        layout.orientation = LinearLayout.HORIZONTAL
-
-        val params = layout.layoutParams as ViewGroup.MarginLayoutParams
-        params.setMargins(16, 4, 16, 4)
-
-        layout.layoutParams = params
-
-        val stateButton = Button(this)
-        stateButton.setText("Cross")
-
-        layout.addView(stateButton)
-        todoContainer.addView(layout)
+        addButton.setOnClickListener {
+            val widget = TodoWidget(this, todoContainer)
+            activeTodoWidget = widget
+            todoList.add(widget)
+        }
     }
 }
